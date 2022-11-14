3b741933c82716c438d25a503fcd5b56dc3d50e1
yousef5 <aboyousef555eng@gmail.com>
2022-11-14 11:01:24 +0200

    update test resize function

--- src/tests/helpers/imageResize.Spec.ts
+++ src/tests/helpers/imageResize.Spec.ts
@@ -4,31 +4,24 @@ import { promises as fsPromises } from "fs";
 import sizeOf from "image-size";
 
 const imageName = "fjord";
-const imageWidth = 50;
-const imageHiegth = 100;
+const imageWidth = 300;
+const imageHiegth = 400;
 const fullSizeImgPath = path.join(process.cwd(), "assets", "fullsize", `${imageName}.jpg`);
 const imgThumbPath = path.join(process.cwd(), "assets", "thumb", `${imageName}-${imageWidth}-${imageHiegth}.jpg`);
 describe("image Resize function", (): void => {
     it("check img after resizing it in thumb Folder", async (): Promise<void> => {
         await resizeImage(fullSizeImgPath, imgThumbPath, imageWidth, imageHiegth);
-
         const checkThumbImage = await fsPromises.stat(imgThumbPath).catch(() => {
             return null;
         });
-
         expect(checkThumbImage).toBeTruthy();
     });
 
-    it("check the height of image after resize", async (): Promise<void> => {
-        await resizeImage(fullSizeImgPath, imgThumbPath, imageWidth, imageHiegth);
-        sizeOf(imgThumbPath, (err, dim) => {
-            expect(dim?.height).toBe(imageHiegth);
-        });
-    });
-    it("check the width of image after resize", async (): Promise<void> => {
+    it("check the width and height of image after resize", async (): Promise<void> => {
         await resizeImage(fullSizeImgPath, imgThumbPath, imageWidth, imageHiegth);
         sizeOf(imgThumbPath, (err, dim) => {
             expect(dim?.width).toBe(imageWidth);
+            expect(dim?.height).toBe(imageHiegth);
         });
     });
 });
